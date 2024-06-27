510cea516bdb4f4590d3d18a032b3f12c87d5496
unknown <sjhuh1999@gmail.com>
2024-06-27 15:05:56 -0500

    Part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -2,7 +2,7 @@ const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
 
-// TODO 1.1a: Define candidateName // 
+// TODO 1.1a: Define candidateName //
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = ("Who was the first American woman in space? ");
@@ -12,11 +12,20 @@ let candidateAnswer = "";
 
 //TODO: Variables for Part 2
 let questions = [
-  "",
-  "",
+  "Who was the first American woman in space? " ,
+  "True or false: 5 kilometers == 5000 meters? " ,
+  "(5+3)/2*10 = ? " ,
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? " ,
+  "What is the minimum crew size for the ISS? ",
 ];
-let correctAnswers;
-let candidateAnswers;
+let correctAnswers = [
+  "Sally Ride",
+  "true",
+  "40",
+  "Trajectory",
+  "3"
+]
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -26,17 +35,26 @@ candidateName = input.question("Enter your name: ")
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question)
+for (let i=0; i < questions.length; i++){
+  candidateAnswers[i] = input.question(questions[i]);
+}
 
 }
 
 function gradeQuiz(candidateAnswers) {
 console.log(candidateAnswer)
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer === "Sally Ride") {
-  console.log("Correct answer")
-} else {
-  console.log("incorrect answer")
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //
+for (let i=0; i< questions.length; i++){
+  console.log(`${i+1} ${questions[i]}`)
+  console.log(`${candidateName} Answer: ${candidateAnswers[i]}`);
+  console.log(`Correct Answer: ${correctAnswers[i]}`)
+
+  if (candidateAnswers[i] === correctAnswers[i]){
+    console.log(`Correct answer for question ${i+1}`)
+
+  } else {
+    console.log(`Incorrect answer for question ${i+1}`)
+  }
 }
 
 
